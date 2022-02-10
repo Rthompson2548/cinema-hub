477215ae3abf3d46c46781f887de5cf5f10ebd87
reillythompson <reilly.thompson@webwizrd.com>
2022-02-09 16:31:16 -0800

    implemented click event listener that changes the total number of tickets and price when a user selects a seat

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const container = document.querySelector(".container");
-
 /** `:not(.occupied)` => only creates a node list of all seats without occupied class */
 const seats = document.querySelectorAll(".row .seat:not(.occupied)");
-
 const count = document.getElementById("count");
 const total = document.getElementById("total");
-
 /** dropdown menu of movies */
 const movieSelect = document.getElementById("movie");
-/** `+` turns the element from a string into a number */
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
+
+/** updates total & count */
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+/** event handler for changing an unoccupied seat's class to `selected` if the user clicks on it */
+container.addEventListener('click', event => {
+  if (
+    event.target.classList.contains('seat') &&
+    !event.target.classList.contains('occupied')
+  ) {
+    event.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
+
+// Initial count and total set
+updateSelectedCount();
