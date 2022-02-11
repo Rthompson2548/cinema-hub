ccb42db13cf4341b7a0a88b64539e107eb5fe0fb
reillythompson <reilly.thompson@webwizrd.com>
2022-02-10 18:23:51 -0800

    enabled data to be saved and displayed on screen on reload

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
-/** dropdown menu of movies */
 const movieSelect = document.getElementById("movie");
+
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 /** save the selected movie index & price */
@@ -23,22 +25,44 @@ function updateSelectedCount() {
     return [...seats].indexOf(seat);
   });
 
-  /** console.log(`seats index: ${seatsIndex}`) => [seat1, seat3, seat7, ...] */
   localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
   
-
   const selectedSeatsCount = selectedSeats.length;
-
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
-
 }
+
+function populateUI() {
+  /** retrieves the selectedSeats that were stored in localStorage & reformats them into a JSON object */
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  /** check if there is any data in selectedSeats */
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      /** checks if the returned index a positive number (means that it exists) */
+      if (selectedSeats.indexOf(index) > -1) {
+        /** uses add() to save the seat's class into localStorage permanently */
+        seat.classList.add("selected");
+      }
+    })
+  }
+
+  /** uses selectedMovieIndex that was set to store movie index into localStorage */
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  
+  /** checks if the movie's index is already in the current localStorage */
+  if (selectedMovieIndex !== null) {
+    /** sets the movie into localStorage permanently */
+    movieSelect.selectedIndex === selectedMovieIndex;
+    movieSelect.innerText = movieSelect;
+  }
+};
 
 
 /** changes ticketPrice to the selected movie's ticket price */
 movieSelect.addEventListener("change", (event) => {
   ticketPrice = +event.target.value;
-  setMovieData(`current movie index: ${event.target.selectedIndex} ${event.target.value}`);
+  setMovieData(`current movie: ${event.target.selectedIndex} ${event.target.value}`);
 })
 
 /** event handler for changing an unoccupied seat's class to `selected` if the user clicks on it */
@@ -47,8 +71,13 @@ container.addEventListener('click', event => {
     event.target.classList.contains('seat') &&
     !event.target.classList.contains('occupied')
   ) {
+    /** uses `toggle` to select/deselect the seat class on click */
     event.target.classList.toggle('selected');
 
     updateSelectedCount();
   }
 });
+
+
+/** sets the initial user's count and total */
+updateSelectedCount();
