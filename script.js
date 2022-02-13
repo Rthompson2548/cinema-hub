76aed7ed853d98f3ab31d13bbdb94eb28db9894a
reillythompson <reilly.thompson@webwizrd.com>
2022-02-12 20:18:41 -0800

    updated event listener for order button to display confirmation message on page

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
+const placeOrder = document.getElementById("place-order");
+const cinemaHub = document.getElementById("cinema-hub");
+const confirmationMessage = document.getElementById("confirm-order-message")
 
 /** displays the current date on the page */
 n = new Date();
@@ -43,7 +46,7 @@ function populateUI() {
   /** retrieves the selectedSeats that were stored in localStorage & reformats them into a JSON object */
   const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
 
-  /** check if there is any data in selectedSeats */
+  /** checks if there is any data in selectedSeats */
   if (selectedSeats !== null && selectedSeats.length > 0) {
     seats.forEach((seat, index) => {
       /** checks if the returned index a positive number (means that it exists) */
@@ -79,10 +82,21 @@ container.addEventListener('click', event => {
 });
 
 
+/** will display a message of order confirmation when use clicks the "place order" button */
+placeOrder.addEventListener("click", (event) => {
+  if (
+    event.target.classList.contains("place-order-button")) { 
+    /** removes all other elements from page and just displays the confirmation message */
+    cinemaHub.style.display = "none";
+    confirmationMessage.classList.toggle("order-confirmation-message")
+  }
+})
+
 /** changes ticketPrice to the selected movie's ticket price */
 movieSelect.addEventListener("change", (event) => {
   ticketPrice = +event.target.value;
-  // window.location.reload();
+  /** reloads page on click to clear selected seats from previous movie */
+  window.location.reload();
   setMovieData(event.target.selectedIndex, event.target.value);
   updateOrder();
 })
